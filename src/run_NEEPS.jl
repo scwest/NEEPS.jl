327cf920a0fd85a30c1ee1466ec5b87bd4f7b9d7
swest <seanchristianwest@unomaha.edu>
2018-10-12 10:37:37 -0500

    f7

--- src/run_NEEPS.jl
+++ src/run_NEEPS.jl
@@ -9,11 +9,6 @@ addprocs(num_workers)
 null_ps, lowest_pvals = parallel_null_and_curves(null_size, days_to_event,
 event, min_threshold, max_threshold, expression_mat, num_workers)
 
-println("Null Pvals")
-println(null_ps)
-println("lowest pvals")
-println(lowest_pvals)
-
 ordered_neep_adj_pvals = generate_neep_all(null_ps, lowest_pvals)
 
-export_to_file(clinical_patient_order, ordered_neep_adj_pvals, output_filename)
+export_to_file(element_order, ordered_neep_adj_pvals, output_filename)
