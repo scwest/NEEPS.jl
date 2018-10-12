8be5c4e62a480a0c3cc7387d567d58fe7df6d971
swest <seanchristianwest@unomaha.edu>
2018-10-12 13:15:44 -0500

    vis_start

--- src/NEEPS.jl
+++ src/NEEPS.jl
@@ -7,12 +7,16 @@ export
     export_to_file,
     parallel_null_and_curves,
     generate_neep_all,
-    get_input
+    get_input,
+    null_vs_lowest,
+    upload_clinical,
+    upload_expression
 
 include("command_line_arguments.jl")
 include("survival_log_rank_pvals.jl")
 include("parallel_jobs.jl")
 include("transformation.jl")
 include("export_to_file.jl")
+include("visualizations.jl")
 
 end
---
+++ src/visualizations.jl
@@ -0,0 +1,26 @@
+using DataFrames
+using StatPlots
+using Plots
+using GR
+
+function null_vs_lowest(null_ps, lowest_pvals, outfilename)
+    gr()
+    null_ps = null_ps[1:length(lowest_pvals)]
+    lpname = Array{String, 1}(length(lowest_pvals))
+    lpname[:] = "Lowest p-value"
+    lpboth = hcat(lowest_pvals, lpname)
+    nuname = Array{String, 1}(length(null_ps))
+    nuname[:] = "Null"
+    nuboth = hcat(null_ps, nuname)
+    ar = vcat(lpboth, nuboth)
+    df = DataFrame(ar)
+    rename!(df, :x1 => :vals)
+    rename!(df, :x2 => :dist)
+
+    beginprint(outfilename)
+    @df df density(:vals, group=(:dist))
+    plot!(xlabel="p-value", ylabel="density")
+    plot!(fmt=:svg)
+    endprint()
+
+end
