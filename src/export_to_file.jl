7605bc536d265580966f0c7fdfed66c4dba56227
swest <seanchristianwest@unomaha.edu>
2018-10-17 16:50:18 -0500

    s30

--- src/export_to_file.jl
+++ src/export_to_file.jl
@@ -2,7 +2,7 @@ function export_to_file(element_order, ordered_neep_adj_pvals, directions, outpu
     print("exporting p-values")
     open(output_filename, "w") do outfile
         for (element, pval, direction) in zip(element_order, ordered_neep_adj_pvals, directions)
-            dstring = ifelse(direction, "high expression survived longer", "low expression survived longer")
+            dstring = ifelse(direction > 0, "high expression survived longer", "low expression survived longer")
             write(outfile, "$element\t$pval\t$dstring\n")
         end
     end
