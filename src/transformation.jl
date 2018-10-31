d70b823f9b60126c6d85f8d39080f4509a1279bc
swest <seanchristianwest@unomaha.edu>
2018-10-31 13:00:05 -0500

    n12

--- src/transformation.jl
+++ src/transformation.jl
@@ -40,6 +40,7 @@ function generate_neep_all(null_ps, lowest_pvals)
 end
 
 function adjust_neep_all(ordered_neep_pvals)
+    ordered_neep_adj_pvals = Array{Float64, 1}(undef, length(ordered_neep_pvals))
     for i in 1:length(ordered_neep_pvals)
         ordered_neep_adj_pvals[i] = ordered_neep_pvals * length(ordered_neep_pvals) / i
     end
